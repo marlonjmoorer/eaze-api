5067849fe1259577114a352d96e6c7e8e8d6c155
marlonmoorer <marlonjmoorer@gmail.com>
2018-02-23 21:55:16 -0500

    deployment setup fixes

--- eaze/settings.py
+++ eaze/settings.py
@@ -140,6 +140,6 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
