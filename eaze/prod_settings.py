9b120e613e5d5678f07ce6bea6827a34dc25e534
marlonmoorer <marlonjmoorer@gmail.com>
2018-02-23 22:12:51 -0500

    deployment setup fixes

--- eaze/prod_settings.py
+++ eaze/prod_settings.py
@@ -1,13 +1,16 @@
 from eaze.settings import  *
 
+import dj_database_url
 
-DATABASES={
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST':os.environ['HOST'],
-        'PORT':os.environ['PORT'],
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+# DATABASES={
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ['NAME'],
+#         'USER': os.environ['USER'],
+#         'PASSWORD': os.environ['PASSWORD'],
+#         'HOST':os.environ['HOST'],
+#         'PORT':os.environ['PORT'],
+#     }
+# }
