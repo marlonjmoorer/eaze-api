eab89a938655691a090529a25b922fc5c5ab9fa4
marlonmoorer <marlonjmoorer@gmail.com>
2018-04-18 21:57:11 -0400

    settings update

--- eaze/settings/prod.py
+++ eaze/settings/prod.py
@@ -1,4 +1,4 @@
-from base import  *
+from .base import  *
 
 import dj_database_url
 
@@ -6,13 +6,3 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DEBUG=False
 ALLOWED_HOSTS = ['eaze-app-api.herokuapp.com']
 SECRET_KEY= os.environ["SECRET"]
-# DATABASES={
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ['NAME'],
-#         'USER': os.environ['USER'],
-#         'PASSWORD': os.environ['PASSWORD'],
-#         'HOST':os.environ['HOST'],
-#         'PORT':os.environ['PORT'],
-#     }
-# }
