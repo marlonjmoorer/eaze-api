72ea785fea810d4dd9d06be41b30baef1f952196
marlonmoorer <marlonjmoorer@gmail.com>
2018-04-20 05:44:27 -0400

    debuging

--- eaze/settings/prod.py
+++ eaze/settings/prod.py
@@ -3,6 +3,6 @@ from .base import  *
 import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-DEBUG=False
+DEBUG=True
 ALLOWED_HOSTS = ['eaze-app-api.herokuapp.com']
 SECRET_KEY= os.environ["SECRET"]
